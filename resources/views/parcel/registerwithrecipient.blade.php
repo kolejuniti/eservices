b512a307dcb8fecaf64f146640c097f3aa5de6af
faizulsoknan <mohdfaizul@uniti.edu.my>
2025-12-04 14:33:13 +0800

    fix the issued about cod_id

--- resources/views/parcel/registerwithrecipient.blade.php
+++ resources/views/parcel/registerwithrecipient.blade.php
@@ -14,6 +14,16 @@
                     {{ session('success') }}
                 </div>
             @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <strong>Validation Errors:</strong>
+                    <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">{{ __('Pendaftaran Parcel') }}</div>
                 <div class="card-body">
